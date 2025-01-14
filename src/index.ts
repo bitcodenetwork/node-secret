19e6917c86cadb6d96242ca7b6e9bc402b9460bc
Catur Wicaksono <bitcodenetwork@gmail.com>
2025-01-14 16:12:26 +0700

    add scrypt method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createCipheriv, createDecipheriv, createHash } from "crypto";
+import { createCipheriv, createDecipheriv, createHash, randomBytes, scryptSync } from "crypto";
 
 type Constructor = {
   algorithm?: "sha256" | "sha512";
@@ -6,24 +6,15 @@ type Constructor = {
   secretIV?: string;
 }
 
-export class Secret {
-  constructor({ algorithm, secret, secretIV }: Constructor) {
+class Secret {
+  constructor(params?: Constructor) {
 
-    let defaultSecret = '';
-    let defaultSecretIV = '';
+    const { algorithm, secret, secretIV } = params || {};
 
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
+    const defaultSecret = randomBytes(5).toString("hex");
+    const defaultSecretIV = randomBytes(5).toString("hex");
 
-    let counter = 0;
-
-    while (counter < 5) {
-      defaultSecret += characters.charAt(Math.floor(Math.random() * charactersLength));
-      defaultSecretIV += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-
-    this.algorithm = algorithm || "sha256";
+    this.algorithm = algorithm || process.env.CRYPTO_ALGORITHM || "sha256";
     this.secret = secret || process.env.CRYPTO_SECRET || defaultSecret;
     this.secretIV = secretIV || process.env.CRYPTO_SECRET_IV || defaultSecretIV;
   }
@@ -83,4 +74,45 @@ export class Secret {
       throw error;
     }
   }
+
+  public encode(data: string, type: "ascii" | "base64" | "base64url" | "binary" | "hex" | "latin1" | "ucs2" | "ucs-2" | "utf16le" | "utf-16le" | "utf8" | "utf-8" = "base64"): string | null {
+    return Buffer.from(data).toString(type);
+  }
+
+  public decode(data: string, type: "ascii" | "base64" | "base64url" | "binary" | "hex" | "latin1" | "ucs2" | "ucs-2" | "utf16le" | "utf-16le" | "utf8" | "utf-8" = "base64"): string | null {
+    return Buffer.from(data, type).toString('ascii');
+  }
+
+  public scrypt(data: string, options?: { salt?: string, type?: "hex" | "base64" | "base64url" }): string | null {
+    const type = options?.type || "base64";
+    const salt = options?.salt || randomBytes(5).toString("hex");
+    const encodedType = this.encode(type, "base64");
+    const encodedSalt = this.encode(salt, "base64");
+    const key = this.scryptKey(data, salt, type);
+    const hash = encodedType + "$" + encodedSalt + "$" + key;
+
+    return hash;
+  }
+
+  public scryptCompare(data: string, hash: string): boolean {
+    const [encodedType, encodedSalt, key] = hash.split("$");
+    const type: any = this.decode(encodedType, "base64");
+    const salt: any = this.decode(encodedSalt, "base64");
+
+    const comparedKey = this.scryptKey(data, salt, type);
+
+    return comparedKey === key;
+  }
+
+  private scryptKey(data: string, salt: string, type: "hex" | "base64" | "base64url"): string {
+    const scryptOptions = {
+      N: 2 ** 14,
+      r: 8,
+      p: 1
+    };
+
+    return scryptSync(data, salt, 32, scryptOptions).toString(type ?? "base64");
+  }
 }
+
+export default Secret;
